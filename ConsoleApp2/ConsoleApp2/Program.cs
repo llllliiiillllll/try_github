bea97a66eb3dfcce96244f88b093f2111511f8ad
llllliiiillllll <mae_do@occ.co.jp>
2019-05-08 10:22:16 +0900

    aaaa

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace ConsoleApp2
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("画面を閉じるには何かキーを入力してください。");
+            Console.ReadKey();
         }
     }
 }
